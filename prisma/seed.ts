5d374cde9193d615f42fa65fb56b692af3ac1c88
Jack Gray <jgrayau@gmail.com>
2022-03-02 17:11:48 -0500

    Building schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,33 +4,61 @@ import faker from 'faker'
 const prisma = new PrismaClient()
 
 const NUMBER_OF_USERS = 10
-const MAX_POSTS = 10
-const MAX_PROFILES = 2
+const NUMBER_OF_PROJECTS = 6
+const NUMBER_OF_SESSIONS = 3
+const MAX_SESSIONS = 3
+const MAX_PROJECTS = 3
 
 const users: Prisma.UserCreateInput[] = Array.from({
   length: NUMBER_OF_USERS,
 }).map((_, i) => ({
   email: faker.internet.email(),
   name: faker.name.firstName(),
-  posts: {
+  projects: {
     createMany: {
       data: Array.from({
-        length: faker.datatype.number({ min: 0, max: MAX_POSTS }),
+        length: faker.datatype.number({ min: 0, max: MAX_PROJECTS }),
       }).map(() => ({
         content: faker.lorem.paragraphs(),
         title: faker.lorem.words(),
       })),
     },
   },
-  profiles: {
+  sessions: {
     createMany: {
       data: Array.from({
-        length: faker.datatype.number({ min: 1, max: MAX_PROFILES }),
+        length: faker.datatype.number({ min: 1, max: MAX_SESSIONS }),
       }).map(() => ({
         bio: faker.lorem.paragraph(),
       })),
     },
   },
+}))
+
+const projects: Prisma.ProjectCreateInput[] = Array.from({
+  length: NUMBER_OF_PROJECTS,
+}).map((_, i) => ({
+  name: faker.title(),
+  sessions: { 
+    createMany: {
+      data: Array.from({
+        length: faker.datatype.number({ min: 0, max: MAX_SESSIONS})
+      })
+    }
+  }
+}))
+
+const sessions: Prisma.SessionCreateInput[] = Array.from({
+  length: NUMBER_OF_SESSIONS,
+}).map((_, i) => ({
+  session_id: faker.lorem.number(),
+  subject: {
+    createOne: {
+      data: {
+        subject_id: 'test'
+      }
+    }
+  }
 }))
 
 async function main() {
@@ -40,6 +68,20 @@ async function main() {
         data: user,
       }),
     ),
+  ),
+  await prisma.$transaction(
+    projects.map((project) => 
+      prisma.project.create({
+        data: project,
+      })
+    )
+  ),
+  await prisma.$transaction(
+    sessions.map((session) => 
+      prisma.session.create({
+        data: session,
+      })
+    )
   )
 }
 
